f7ad24cb1ecbf46a2765430b40069da9adc03bd9
Tylor Sherman <tylor@denimandsteel.com>
2014-05-07 12:15:41 -0700

    Put text on logo

--- tangents/control.js
+++ tangents/control.js
@@ -1,26 +1,47 @@
 var element = d3.select(".weareyvr-mark");
 var vis = d3.select(".weareyvr-mark svg");
 
+var text = "black";
 var radius = 50;
 var topLeft = 50;
 var topRight = 50;
 var bottomRight = 0;
 var bottomLeft = 0;
-var colour  = "black";
+var colour  = "#4095c5";
 
 // Controls
 d3.selectAll('input[type="range"], input[type="radio"]').on("change", function change() {
   eval(this.name + ' = "' + this.value + '"'); // DIRTY!
-  console.log(this.name + ' = "' + this.value + '"');
+  // console.log(this.name + ' = "' + this.value + '"');
   // var transition = vis.transition().duration(250);
   // // var delay = function(d, i) { return i * 50; };
   // transition.selectAll(".one")
   //   // .delay(delay)
   //   .attr("transform", "translate(" + one.x + ',' + one.y + ")");
 
-
   // Having troubling finding a nice data driven way to do this, since
   // each arc and line is kind of unique, so just remove all and redraw.
+
+  document.body.style.backgroundColor = text === "white" ? "#ccc" : "white";
   vis.selectAll('*').remove();
-  drawMark(vis, element.attr('data-height'), colour,  radius, topLeft, topRight, bottomRight, bottomLeft);
+  drawMark(vis, element.attr('data-height'), text, colour,  radius, topLeft, topRight, bottomRight, bottomLeft);
+});
+
+d3.selectAll('input[type="text"]').on("keyup", function change() {
+  var hashed = hex_md5(this.value);
+  var crc = (parseInt(hashed, 16) % 9999999999).toString(); // Get a 10 digit number
+  radius = parseInt(crc.slice(0, 2), 10) || 0;
+  topLeft = parseInt(crc.slice(2, 4), 10) || 0;
+  topRight = parseInt(crc.slice(4, 6), 10) || 0;
+  bottomRight = parseInt(crc.slice(6, 8), 10) || 0;
+  bottomLeft = parseInt(crc.slice(8, 10), 10) || 0;
+
+  d3.select('input[name="radius"]').property('value', radius)
+  d3.select('input[name="topLeft"]').property('value', topLeft)
+  d3.select('input[name="topRight"]').property('value', topRight)
+  d3.select('input[name="bottomRight"]').property('value', bottomRight)
+  d3.select('input[name="bottomLeft"]').property('value', bottomLeft)
+
+  vis.selectAll('*').remove();
+  drawMark(vis, element.attr('data-height'), text, colour,  radius, topLeft, topRight, bottomRight, bottomLeft);
 });
--- tangents/weareyvr-mark.js
+++ tangents/weareyvr-mark.js
@@ -1,15 +1,15 @@
 d3.selectAll(".weareyvr-mark").each(function() {
   var element = d3.select(this);
-  drawMark(element.append("svg"), element.attr('data-height'), element.attr('data-colour'),  element.attr('data-radius'), element.attr('data-one'), element.attr('data-two'), element.attr('data-three'), element.attr('data-four'));
+  drawMark(element.append("svg"), element.attr('data-height'), element.attr('data-text'), element.attr('data-colour'),  element.attr('data-radius'), element.attr('data-one'), element.attr('data-two'), element.attr('data-three'), element.attr('data-four'));
 });
 
-function drawMark(vis, height, colour, radius, one, two, three, four) {
+function drawMark(vis, height, text, colour, radius, one, two, three, four) {
   var width = height * 0.95;
   var radius = ((parseInt(radius, 10)*70/100)+5)*height/500;
   var stroke = (3/500)*height;
   if (stroke < 1) stroke = 1;
 
-  vis.attr('width', width).attr('height', height);
+  vis.attr('width', width+160).attr('height', height);
 
   // Top left.
   var one = parseInt(one, 10);
@@ -25,7 +25,7 @@ function drawMark(vis, height, colour, radius, one, two, three, four) {
     radius: radius,
     x: (width - radius - stroke) - (two > 50 ? ((two-50)/50)*1.15*(width/2-2*radius) : 0),
     y: (radius + stroke) - (two < 50 ? ((two-50)/50)*2*(height/2-2*radius) : 0),
-  }
+  };
 
   // Bottom right.
   var three = parseInt(three, 10);
@@ -41,7 +41,7 @@ function drawMark(vis, height, colour, radius, one, two, three, four) {
     radius: radius,
     x: (radius + stroke) - (four < 50 ? ((four-50)/50)*1.15*(width/2-2*radius) : 0),
     y: (height - radius - stroke) - (four > 50 ? ((four-50)/50)*2*(height/2-2*radius) : 0),
-  }
+  };
 
   // Port of: http://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Tangents_between_two_circles
   var tangent = function(c1, c2, tangentIndex) {
@@ -76,18 +76,20 @@ function drawMark(vis, height, colour, radius, one, two, three, four) {
           y1: c1.y + c1.radius * ny,
           x2: c2.x + sign1 * c2.radius * nx,
           y2: c2.y + sign1 * c2.radius * ny,
-        }
+        };
         i++;
       }
     }
     return res[tangentIndex];
   }
 
+  var mark = vis.append("g").attr("transform", "translate(80,0)");
+
   // Lines
 
   // Top left to top right.
   var tangentTopLeftToTopRight = tangent(topLeft, topRight, 1);
-  vis.append("line")
+  mark.append("line")
     .attr("x1", tangentTopLeftToTopRight.x1)
     .attr("y1", tangentTopLeftToTopRight.y1)
     .attr("x2", tangentTopLeftToTopRight.x2)
@@ -97,7 +99,7 @@ function drawMark(vis, height, colour, radius, one, two, three, four) {
 
   // Top right to bottom left.
   var tangentTopRightToBottomLeft = tangent(topRight, bottomLeft, 3);
-  vis.append("line")
+  mark.append("line")
     .attr("x1", tangentTopRightToBottomLeft.x1)
     .attr("y1", tangentTopRightToBottomLeft.y1)
     .attr("x2", tangentTopRightToBottomLeft.x2)
@@ -107,7 +109,7 @@ function drawMark(vis, height, colour, radius, one, two, three, four) {
 
   // Bottom left to bottom right.
   var tangentBottomLeftToBottomRight = tangent(bottomLeft, bottomRight, 0);
-  vis.append("line")
+  mark.append("line")
     .attr("x1", tangentBottomLeftToBottomRight.x1)
     .attr("y1", tangentBottomLeftToBottomRight.y1)
     .attr("x2", tangentBottomLeftToBottomRight.x2)
@@ -117,7 +119,7 @@ function drawMark(vis, height, colour, radius, one, two, three, four) {
 
   // Bottom right to top left.
   var tangentBottomRightToTopLeft = tangent(bottomRight, topLeft, 2);
-  vis.append("line")
+  mark.append("line")
     .attr("x1", tangentBottomRightToTopLeft.x1)
     .attr("y1", tangentBottomRightToTopLeft.y1)
     .attr("x2", tangentBottomRightToTopLeft.x2)
@@ -132,9 +134,9 @@ function drawMark(vis, height, colour, radius, one, two, three, four) {
     .innerRadius(topLeft.radius - stroke/2)
     .outerRadius(topLeft.radius - stroke/2 + stroke)
     .startAngle(Math.PI + Math.atan((topLeft.x - tangentBottomRightToTopLeft.x2) / (tangentBottomRightToTopLeft.y2 - topLeft.y)))
-    .endAngle(2 * Math.PI + Math.atan((topLeft.x - tangentTopLeftToTopRight.x1) / (tangentTopLeftToTopRight.y1 - topLeft.y)))
+    .endAngle(2 * Math.PI + Math.atan((topLeft.x - tangentTopLeftToTopRight.x1) / (tangentTopLeftToTopRight.y1 - topLeft.y)));
 
-  vis.append("path")
+  mark.append("path")
     .attr("d", arc)
     .style("fill", colour)
     .attr("transform", "translate(" + topLeft.x + ',' + topLeft.y + ")")
@@ -144,34 +146,56 @@ function drawMark(vis, height, colour, radius, one, two, three, four) {
     .innerRadius(topRight.radius - stroke/2)
     .outerRadius(topRight.radius - stroke/2 + stroke)
     .startAngle(Math.atan((topRight.x - tangentTopLeftToTopRight.x2) / (tangentTopLeftToTopRight.y2 - topRight.y)))
-    .endAngle(Math.PI + Math.atan((topRight.x - tangentTopRightToBottomLeft.x1) / (tangentTopRightToBottomLeft.y1 - topRight.y)))
+    .endAngle(Math.PI + Math.atan((topRight.x - tangentTopRightToBottomLeft.x1) / (tangentTopRightToBottomLeft.y1 - topRight.y)));
 
-  vis.append("path")
+  mark.append("path")
     .attr("d", arc)
     .style("fill", colour)
-    .attr("transform", "translate(" + topRight.x + ',' + topRight.y + ")")
+    .attr("transform", "translate(" + topRight.x + ',' + topRight.y + ")");
 
   // Bottom right.
   var arc = d3.svg.arc()
     .innerRadius(bottomRight.radius - stroke/2)
     .outerRadius(bottomRight.radius - stroke/2 + stroke)
     .startAngle(Math.atan((bottomRight.x - tangentBottomRightToTopLeft.x1) / (tangentBottomRightToTopLeft.y1 - bottomRight.y)))
-    .endAngle(Math.PI + Math.atan((bottomRight.x - tangentBottomLeftToBottomRight.x2) / (tangentBottomLeftToBottomRight.y2 - bottomRight.y)))
+    .endAngle(Math.PI + Math.atan((bottomRight.x - tangentBottomLeftToBottomRight.x2) / (tangentBottomLeftToBottomRight.y2 - bottomRight.y)));
 
-  vis.append("path")
+  mark.append("path")
     .attr("d", arc)
     .style("fill", colour)
-    .attr("transform", "translate(" + bottomRight.x + ',' + bottomRight.y + ")")
+    .attr("transform", "translate(" + bottomRight.x + ',' + bottomRight.y + ")");
 
   // Bottom left.
   var arc = d3.svg.arc()
     .innerRadius(bottomLeft.radius - stroke/2)
     .outerRadius(bottomLeft.radius - stroke/2 + stroke)
     .startAngle(Math.PI + Math.atan((bottomLeft.x - tangentBottomLeftToBottomRight.x1) / (tangentBottomLeftToBottomRight.y1 - bottomLeft.y)))
-    .endAngle(2 * Math.PI + Math.atan((bottomLeft.x - tangentTopRightToBottomLeft.x2) / (tangentTopRightToBottomLeft.y2 - bottomLeft.y)))
+    .endAngle(2 * Math.PI + Math.atan((bottomLeft.x - tangentTopRightToBottomLeft.x2) / (tangentTopRightToBottomLeft.y2 - bottomLeft.y)));
 
-  vis.append("path")
+  mark.append("path")
     .attr("d", arc)
     .style("fill", colour)
-    .attr("transform", "translate(" + bottomLeft.x + ',' + bottomLeft.y + ")")
+    .attr("transform", "translate(" + bottomLeft.x + ',' + bottomLeft.y + ")");
+
+  if (text !== '') {
+    var letterforms = [
+      { d: "M65.01 0.264l20.835 75.032L106.944 0h11.34L91.252 92.307H80.043L59.076 16.879 38.242 92.307H26.901L0 0h12l20.703 75.823 20.835-75.56H65.01z" },
+      { d: "M119.997 60.263c0-22.022 10.681-33.758 30.593-33.758 18.988 0 27.428 13.187 27.428 30.989 0 1.978-0.132 3.956-0.264 6.065h-46.548c0.791 15.561 7.384 21.23 19.384 21.23 9.23 0 14.9-2.373 18.856-11.208l8.439 4.352c-5.143 11.208-14.901 15.955-27.56 15.955C131.073 93.889 119.997 83.472 119.997 60.263zM131.337 54.856h35.472c-0.528-13.846-6.593-19.384-16.747-19.384C138.985 35.472 132.523 40.879 131.337 54.856z" },
+      { d: "M251.333 73.45h-42.329l-6.33 18.856h-12L222.982 0h14.769l32.307 92.307h-12.396L251.333 73.45zM212.301 63.56h35.604l-17.934-52.878L212.301 63.56z" },
+      { d: "M283.771 28.088h9.758l1.187 13.582c4.483-10.549 13.582-15.428 24.527-14.637v10.813c-15.824-1.583-23.736 7.121-24.132 23.604v30.857h-11.34V28.088z" },
+      { d: "M327.945 60.263c0-22.022 10.681-33.758 30.593-33.758 18.989 0 27.429 13.187 27.429 30.989 0 1.978-0.133 3.956-0.265 6.065h-46.549c0.792 15.561 7.385 21.23 19.385 21.23 9.23 0 14.901-2.373 18.856-11.208l8.439 4.352c-5.143 11.208-14.9 15.955-27.56 15.955C339.021 93.889 327.945 83.472 327.945 60.263zM339.285 54.856h35.473c-0.527-13.846-6.594-19.384-16.747-19.384C346.933 35.472 340.472 40.879 339.285 54.856z" },
+      { d: "M387.546 0h12.659l26.506 45.626L453.084 0h12.264l-32.967 55.384v36.923h-11.736V55.384L387.546 0z" },
+      { d: "M485.256 0l27.429 81.625L540.245 0h12l-32.308 92.307h-14.769L472.861 0H485.256z" },
+      { d: "M564.77 0h36.659c20.834 0 30.988 10.022 30.988 27.692 0 14.505-6.594 23.736-18.197 27.692L634 92.307h-12.922l-18.726-34.945h-25.846v34.945H564.77V0zM576.506 10.022v37.187h24.791c12.132 0 19.384-6.198 19.384-19.121 0-11.473-6.197-18.066-19.252-18.066H576.506z" },
+    ];
+
+    var letterGroup = vis.append("g").attr("transform", "translate(0,179)");
+    var letters = letterGroup.selectAll("path")
+      .data(letterforms)
+      .enter()
+      .append("path")
+      .style("fill", text);
+      
+    letters.attr("d", function (d) { return d.d; });
+  }
 }
